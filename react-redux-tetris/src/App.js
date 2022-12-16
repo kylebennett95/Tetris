835d2b45b2616a5ea5ac27a2d035f9641fdcbccd
Bennett <BENNEDAV100@wcschools.com>
2022-12-16 16:36:38 -0600

    added initial controls

--- react-redux-tetris/src/App.js
+++ react-redux-tetris/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import GridBoard from './components/GridBoard';
 import NextBlock from './components/NextBlock';
 import ScoreBoard from './components/ScoreBoard';
+import Controls from './components/Controls';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <GridBoard />
       <NextBlock />
       <ScoreBoard />
+      <Controls />
     </div>
   );
 }
